8b30769b3d602abcd0d0c5848da1fd42c96296a5
codegrinders3 <56885842+codegrinders3@users.noreply.github.com>
2019-12-31 19:12:48 +0530

    Update tryservice.service.ts

--- src/app/services/tryservice.service.ts
+++ src/app/services/tryservice.service.ts
@@ -19,51 +19,49 @@ export class TryService {
     search = new URLSearchParams();
     constructor(private http: HttpClient) { }
     
-    getAllCustomers2(url, requestBody): Observable<Customer[]> {
-      console.log('hereee' + url);
+    getCustomerOnParam(url, requestBody): Observable<Customer[]> {
       return this.http.get<Customer[]>(this.customUrl+ url, {params: {payload: requestBody}})
       .pipe(retry(1), catchError(this.handleError)
       )
     }
 
     // HttpClient API post() method => Create employee
-  getFilteredData(url, requestBody): Observable<Customer[]> {
+    getFilteredData(url, requestBody): Observable<Customer[]> {
       //console.log();
       return this.http.get<Customer[]>(this.customUrl+ url, {params: {payload: requestBody}})
       .pipe(retry(1), catchError(this.handleError)
       )
-  }  
+    }  
 
-  getAllCustomers(): Observable<any> {
-    return this.http.get<Customer>(this.customUrl)
-    .pipe(
-      retry(1),
-      catchError(this.handleError)
-    )
-  }
-
-  getCustomer(): Observable<any> {
-      return this.http.get(this.customUrl);
-  }
-  
-  addCustomer(customer: String): Observable<any> {
-    return this.http.post(this.customUrl+ 'addcustomer', customer, httpOptions)
-    .pipe(retry(1), catchError(this.handleError)
-    )
-  }
-
-  
-  // Error handling 
-  handleError(error) {
-    let errorMessage = '';
-    if(error.error instanceof ErrorEvent) {
-      // Get client-side error
-      errorMessage = error.error.message;
-    } else {
-      // Get server-side error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    getAllCustomers(): Observable<any> {
+      return this.http.get<Customer>(this.customUrl)
+      .pipe(retry(1), catchError(this.handleError)
+      )
     }
-    window.alert(errorMessage);
-    return throwError(errorMessage);
- }
+
+    getInsuredCustomers():Observable<any> {
+      return this.http.get<Customer>(this.customUrl+'getAMCDone')
+      .pipe(retry(1), catchError(this.handleError)
+      )
+    }
+    
+    addCustomer(customer: Customer): Observable<any> {
+      return this.http.post(this.customUrl + 'addcustomer', customer, httpOptions)
+      .pipe(retry(1), catchError(this.handleError)
+      )
+    }
+
+    // Error handling 
+    handleError(error) {
+      let errorMessage = '';
+      if(error.error instanceof ErrorEvent) {
+        // Get client-side error
+        errorMessage = error.error.message;
+      } else {
+        // Get server-side error
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      }
+      window.alert(errorMessage);
+      return throwError(errorMessage);
+   }
 }
